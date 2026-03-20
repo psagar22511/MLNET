b0646383f0a692817af0db02f6da895e0dfad43e
SagarPatell <sagar.patel@goodcareit.com>
2026-03-20 10:07:42 +0530

    no message

---
+++ AnomalyDetectionExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using AnomalyDetectionExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Anomaly detection Example
+
+        string dataPath = @"C:\Sagar-PC\Learning\MLNET\AnomalyDetectionExample\NLPTextProcessing.csv";
+        string modelPath = @"C:\Sagar-PC\Learning\MLNET\AnomalyDetectionExample\AnomalyDetectionModel.zip";
+
+        var nlpService = new AnomalyDetectionService(dataPath, modelPath);
+    }
+}
---
+++ RankingExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using RankingExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ranking Example
+
+        string dataPath = @"C:\Sagar-PC\Learning\MLNET\RankingExample\spamdata.csv";
+        string modelPath = @"C:\Sagar-PC\Learning\MLNET\RankingExample\rankingModel.zip";
+
+        var spamDetector = new RankingService(dataPath, modelPath);
+
+        var result = spamDetector.Predict(new RankingData
+        {
+            Feature1 = 2,
+            Feature2 = 0.15f,
+            GroupId = 1
+        });
+
+        Console.WriteLine($"Ranking Score: {result.Score}");
+    }
+}
