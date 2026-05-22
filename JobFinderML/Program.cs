f218aead9062d9d32256128d67cbd022a9858d1f
SagarPatell <sagar.patel@goodcareit.com>
2026-05-22 15:21:35 +0530

    Added Job find project

---
+++ JobFinderML/Program.cs
@@ -0,0 +1,14 @@
+﻿using JobFinderML;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dataPath = @"C:\Sagar-PC\Learning\MLNET\JobFinderML\jobdata.csv";
+        string modelPath = @"C:\Sagar-PC\Learning\MLNET\JobFinderML\jobFinderModel.zip";
+
+        var jobFinder = new JobFinderService(dataPath, modelPath);
+        var result = jobFinder.Predict(".NET Backend Developer", "C# ASP.NET Core Web API Azure");
+        Console.WriteLine($"Relevant: {result.Prediction}");
+    }
+}
